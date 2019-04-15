59b7aa367c030a2ac0b1ec98f445a1c6683762dc
Jeramy Harrison <dbralir@gmail.com>
2019-04-14 23:56:50 -0500

    Added package lib table type

--- src/libs_package.cpp
+++ src/libs_package.cpp
@@ -11,6 +11,21 @@ namespace typedlua::libs {
 void import_package(Scope& scope) {
     auto [node, errors] = parse(R"(
         global require: <T: string>(modname: T): $require(T)
+
+        global package: {
+            config: string
+            cpath: string
+            loaded: { [string]: any }
+            loadlib: (libname: string, funcname: string): any
+            path: string
+            preload: {
+                [string]: (modname: string): [loader: (arg: any): any, arg: any] | string | nil
+            }
+            searchers: {
+                [string]: (modname: string): [loader: (arg: any): any, arg: any] | string | nil
+            }
+            searchpath: (name: string, path: string, sep: string | nil, rep: string | nil): string | [:nil, error: string]
+        }
     )");
 
     if (node && errors.empty()) {
